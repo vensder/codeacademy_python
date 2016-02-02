9b94fa61f222328365be42ef2b80ee866918e87f
vensder <vensder@gmail.com>
2016-02-02 19:56:07 +0300

    min_max changed

--- min_max.py
+++ min_max.py
@@ -7,27 +7,28 @@ def string_to_num(num):
     except ValueError:
         return float(num)
 
-def biggest_number(*args):
-    return max(args)
+def biggest_number(arg):
+    return max(arg)
     
-def smallest_number(*args):
-    return min(args)
+def smallest_number(arg):
+    return min(arg)
 
 def distance_from_zero(arg):
     return abs(arg)
 
+n = 4
+i = 0
+collection = [None] * n
 
-a = raw_input("Input first number:\n")
-b = raw_input("Input second number:\n")
-c = raw_input("Input third number:\n")
-d = raw_input("Input fourth number:\n")
+for i in range (0, n):
+    collection[i] = raw_input("Input number:\n")
 
-a = string_to_num(a)
-b = string_to_num(b)
-c = string_to_num(c)
-d = string_to_num(d)
+print collection
+
+collection = [string_to_num(n) for n in collection]
+print collection
 
 print 'Results:'
-print 'The biggest number is: ' + str(biggest_number(a, b, c, d))
-print 'The smallest number is: ' + str(smallest_number(a, b, c, d))
-print 'Distance from zero of the first number is: %s' % distance_from_zero(a)
+print 'The biggest number is: ' + str(biggest_number(collection))
+print 'The smallest number is: ' + str(smallest_number(collection))
+print 'Distance from zero of the first number is: %s' % distance_from_zero(collection[0])
