5c04c52f12d2563932c616324a5d7c1d8a8a6f52
vensder <vensder@gmail.com>
2016-02-02 19:02:35 +0300

    min_max changed

--- min_max.py
+++ min_max.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+def string_to_num(num):
+    try:
+        return int(num)
+    except ValueError:
+        return float(num)
+
 def biggest_number(*args):
     return max(args)
     
@@ -11,6 +17,17 @@ def distance_from_zero(arg):
     return abs(arg)
 
 
-print biggest_number(-10, -5, 5, 10)
-print smallest_number(-10, -5, 5, 10)
-print distance_from_zero(-10)
+a = raw_input("Input first number:\n")
+b = raw_input("Input second number:\n")
+c = raw_input("Input third number:\n")
+d = raw_input("Input fourth number:\n")
+
+a = string_to_num(a)
+b = string_to_num(b)
+c = string_to_num(c)
+d = string_to_num(d)
+
+print 'Results:'
+print 'The biggest number is: ' + str(biggest_number(a, b, c, d))
+print 'The smallest number is: ' + str(smallest_number(a, b, c, d))
+print 'Distance from zero of the first number is: %s' % distance_from_zero(a)
