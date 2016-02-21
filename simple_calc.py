55aa896ef366c3108a19561a13073ebc06cd5e20
vensder <vensder@gmail.com>
2016-02-21 17:26:15 +0300

    simple calc improoved

--- simple_calc.py
+++ simple_calc.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-n1 = n2 = 0
+def input_to_float(text):
+    n = None
+    while n is not float:
+        try:
+            print(text)
+            n = float(input())
+            return n
+        except ValueError:
+            print('Wrong number format')
 
-def input_numbers(n1,n2):
-    print('Input first number:')
-    n1 = float(input())
-    print('Input second number:')
-    n2 = float(input())
+def input_numbers():
+    n1 = input_to_float('Input first number:')
+    n2 = input_to_float('Input second number:')
     return(n1,n2)
 
 while True:
     print('Choice action ("+", "-", "*" or "/":)')
     op = input()
 
+    n1 = n2 = 0
     if op == '+':
-        n1,n2 = input_numbers(n1,n2)
-        print(str(n1) + ' + ' + str(n2) + ' = ' + str(n1 + n2))
+        n1,n2 = input_numbers()
+        print(str(n1) + ' + '  + str(n2) + ' = ' + str(n1 + n2))
     else:
         break
 
