1363ba58d725f7eb95c06bd59f0341419315711c
vensder <vensder@gmail.com>
2016-02-21 14:50:11 +0300

    simple calc addition added

--- simple_calc.py
+++ simple_calc.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+n1 = n2 = 0
+
+def input_numbers(n1,n2):
+    print('Input first number:')
+    n1 = float(input())
+    print('Input second number:')
+    n2 = float(input())
+    return(n1,n2)
+
 while True:
-    print('Simple Calc')
-    print('Choice action:')
+    print('Choice action ("+", "-", "*" or "/":)')
+    op = input()
+
+    if op == '+':
+        n1,n2 = input_numbers(n1,n2)
+        print(str(n1) + ' + ' + str(n2) + ' = ' + str(n1 + n2))
+    else:
+        break
+
+
+
